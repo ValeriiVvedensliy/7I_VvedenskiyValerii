074e5af50e151996d614bc4fa741035320141538
Valerii <Before you can contribute on GitHub, we need you to verify your email address.  An email containing verification instructions was sent to geekBrains.valerii.vvedenskiy@gmail.com>
2021-07-13 13:44:57 +0300

    setup

---
+++ 7I_VvedenskiyValerii/7I_VvedenskiyValerii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7I_VvedenskiyValerii
+//
+//  Created by Valera Vvedenskiy on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
